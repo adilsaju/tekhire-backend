4972c1a78c25ae50a54145e7e22a8532c7c56f5f
Adil Saju <adilsaju@gmail.com>
2023-02-17 13:16:33 -0800

    data-create commented

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
-const {addData} = require("./scripts/data-create");
-// const studentRoute = require('./routes/studentRoute')
+// const {addData} = require("./scripts/data-create");
 const sample = require('./routes/mainRoute')
 
 
