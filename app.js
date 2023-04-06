d59c0cb7213ba3badad79ecd56804b1d8be396cf
Harshit Punn <harshitpunn@gmail.com>
2023-04-06 14:37:10 -0700

    ADDING merge changes

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-require('dotenv').config();
-const express = require('express');
+require('dotenv').config()
+const express = require('express')
 const app = express();
-const mongoose = require('mongoose');
-const { addData } = require('./scripts/data-create');
-const mainRoute = require('./routes/mainRoute');
-const notificationRoute = require('./routes/notificationRoute');
-const authRoute = require('./routes/authRoute');
+const mongoose = require('mongoose')
+const {addData} = require("./scripts/data-create");
+const mainRoute = require('./routes/mainRoute')
+const notificationRoute = require('./routes/notificationRoute')
+const authRoute = require('./routes/authRoute')
 const session = require('express-session');
 
-const Student = require('./models/technicianModel');
-const Admin = require('./models/employerModel');
-const Job = require('./models/jobModel');
 
-const errorHandler = require('./middlewares/errorMiddleware');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
+
+const Student = require('./models/technicianModel')
+const Admin = require('./models/employerModel')
+const Job = require('./models/jobModel')
+
+
+const errorHandler = require('./middlewares/errorMiddleware')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
 
 //SOCKET FOR CHAT
 const http = require('http');
@@ -23,42 +26,45 @@ const server = http.createServer(app);
 socketServer.init(server);
 //
 
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5001
 
 // mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true } )
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.error('connected to database'));
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true } )
+const db = mongoose.connection 
+db.on('error',(error)=>console.error(error))
+db.once('open',()=>console.error('connected to database'))
+
 
 // Request.requestModel.collection.dropIndexes(function (err, results) {
 //     // Handle errors
 // });
 // addData()
-app.use(cors());
+app.use(cors())
 
 //middleware
-app.use(express.json());
+app.use(express.json())
 // app.disable('view cache');
 // Set up session middleware
-app.use(
-  session({
-    secret: 'abcdefg',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false },
-  })
-);
+app.use(session({
+  secret: 'abcdefg',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
-//router
-app.use('/api/v1/', mainRoute);
-app.use('/api/v1/', notificationRoute);
-app.use('/api/v1/', authRoute);
+
+
+//router 
+app.use("/api/v1/", mainRoute)
+app.use("/api/v1/", notificationRoute)
+app.use("/api/v1/", authRoute)
+
+
 
 // app.use("/login", login)
 
 //error handler middleware
-app.use(errorHandler);
+app.use(errorHandler)
 // app.use(bodyParser()) // support encoded bodies
 // bodyParser.json([])
 // app.use(errorHandler)
@@ -69,6 +75,9 @@ app.use((req, res, next) => {
   });
 });
 
-server.listen(port, () => {
-  console.log('server started');
-});
+server.listen(port, ()=>{
+    console.log('server started');
+})
+
+
+
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,9 @@
+// const Job = require('../models/JobModel.js');
 const express = require('express');
-const employer = require('../models/employerModel');
+const job = require('../models/jobModel')
+const employer = require('../models/employerModel')
 const technician = require('../models/technicianModel');
+<<<<<<< HEAD
 const bcrypt = require('bcrypt');
 const admin = require('../config/firebase.config');
 const { OAuth2Client } = require('google-auth-library');
@@ -46,14 +49,102 @@ const create_Client_Technician = async (req, res, next) => {
         message: 'user added success',
         data: clientUser,
       });
+=======
+const offer = require('../models/offerModel')
+const socketServer = require('../socket2/socket-server');
+const employment = require('../models/employmentModel')
+
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+
+
+  const createTechnician = async (req, res, next) => {
+    console.log("login");
+
+
+
+    
+    res.json("success")
+  
+  };
+
+  const createClient = async (req, res, next) => {
+    console.log("login");
+
+
+
+    
+    res.json("success")
+  
+  };
+
+
+
+
+const createHashPassword = async (password)=>{
+    // const salt = await bcrypt.genSalt()
+    const hashPassword = await bcrypt.hash(password, 10)
+    // console.log("salt", salt);
+    console.log("hashPassword", hashPassword);
+  return hashPassword
+}
+
+
+
+
+const createStudent = ()=>{
+  return async (req, res, next) => {
+    try {
+      const email = req.body.email
+      const password = req.body.password
+   
+      const hashPassword = await createHashPassword(password)
+       //save student to db
+    const studentRequirements1 = await Checklist.studentRequirementsModel.create({flownHours: 123 , balance: 66, licenseType: "cpl", englishProficiency: true, medicalLicense: "abc", radioLicense: "abc", license: "xyz", isRequirementsOk: true })
+       
+    // //VALIDATION LOGIC 
+    // if ( studentRequirements1.flownHours < studentRequirementsCutoff.flownHours){
+    //   studentRequirements1.isRequirementsOk = false
+    // }
+    // if ( studentRequirements1.balance < studentRequirementsCutoff.balance){
+    //   studentRequirements1.isRequirementsOk = false
+    // }
+    // // studentRequirements1.save();
+
+    // if ( !studentRequirements1.license.startsWith("https://") || !studentRequirements1.radioLicense.startsWith("https://") || !studentRequirements1.medicalLicense.startsWith("https://") || !studentRequirements1.englishProficiency.startsWith("https://") ) {
+
+    //   studentRequirements1.isRequirementsOk = false
+
+    // }
+// ===============================
+    
+    const student1 = await Student.studentModel.create({name: req.body.name || "abc" , email: email, password: hashPassword , studentRequirements: studentRequirements1  })
+
+      validateStudentInDb(student1)
+
+       res.status(201).json({error:false, message: "user added success", data: student1})
+    } catch (error) {
+      res.status(500).json({error: true, message: "failed", data: error})
+>>>>>>> parent of 819a433 (Registration-firebase: ADDED code to accomodate firebase registration setup)
     }
+  };
+}
+
+const createAdmin = async (req, res, next) => {
+  try {
+    const email = req.body.email
+    const password = req.body.password
+ 
+    const hashPassword = await createHashPassword(password)
+     //save student to db
+     const adm1 = await Admin.adminModel.create({name: req.body.name || "abc" , email: email, password: hashPassword })
+     res.status(201).json({error:false, message: "user added success", data: adm1})
   } catch (error) {
-    return res
-      .status(500)
-      .json({ error: true, message: 'failed', data: error });
+    res.status(500).json({error: true, message: "failed", data: error})
   }
 };
 
+<<<<<<< HEAD
 const createHashPassword = async (password) => {
   const hashPassword = await bcrypt.hash(password, 10);
   console.log('hashPassword', hashPassword);
@@ -203,3 +294,92 @@ module.exports = {
   google_Login,
   google_User_Login,
 };
+=======
+const login = async (req, res) => {
+
+    //jwt sign
+
+      console.log('login()');
+      user = req.body
+
+     const token =  await jwt.sign({user}, 'secretkey' , {expiresIn: '30000m' }) ;
+    
+
+
+
+    const studentUser = await technician.technicianModel.findOne({
+      email: req.body.email,
+    });
+
+    const adminUser = await employer.employerModel.findOne({
+      email: req.body.email,
+    });
+
+    if (studentUser != null) {
+
+    try {
+      if (
+        await bcrypt.compare(
+          req.body.password,
+          studentUser.password
+        )
+      ) {
+        console.log("scc");
+        res.send({error: false, message: 'login success' , data: studentUser, token: token, isTechnician: true });
+      } else {
+        console.log("pwd incorrect");
+        res.json({error: true, message: 'Not allowed. Password incorrect' });
+      }
+    } catch(error) {
+      return res.status(500).json(error);
+    }
+
+  }else if  (adminUser != null) 
+  {
+
+    try {
+      if (
+        await bcrypt.compare(
+          req.body.password,
+          adminUser.password
+        )
+      ) {
+        console.log("scc");
+        res.send({error: false, message: 'login success' , data: adminUser, token: token, isTechnician: false });
+      } else {
+        console.log("pwd incorrect");
+        res.json({error: true, message: 'Not allowed. Password incorrect' });
+      }
+    } catch(error) {
+      return res.status(500).json(error);
+    }
+
+
+
+
+  }else {
+      return res.status(400).send({error: true, message: 'cannot find email' });
+
+  }
+
+    
+  
+};
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+  login,
+  createTechnician, 
+  createClient,
+  createHashPassword
+}
+
+>>>>>>> parent of 819a433 (Registration-firebase: ADDED code to accomodate firebase registration setup)
