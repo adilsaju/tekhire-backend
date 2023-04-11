b9dbcf4b8831e8eb5acd0333392ea089a359c63c
Harshit Punn <harshitpunn@gmail.com>
2023-04-11 04:14:08 -0700

    FIXING backend  changes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,14 +1,19 @@
 const admin = require('../config/firebase.config');
 class Middleware {
   async decodeToken(req, res, next) {
-    const token = req.headers.authorization.split(' ')[1];
     try {
-      const decodeValue = await admin.auth().verifyIdToken(token);
-      if (decodeValue) {
-        req.user = decodeValue;
+      if (!['/api/v1/register'].includes(req.url)) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        const decodeValue = await admin.auth().verifyIdToken(token);
+        if (decodeValue) {
+          req.user = decodeValue;
+          return next();
+        }
+        return res.json({ message: 'Un authorize' });
+      } else {
         return next();
       }
-      return res.json({ message: 'Un authorize' });
     } catch (e) {
       return res.json({ message: 'Internal Error' });
     }
